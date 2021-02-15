8804a11aa3acf19bd3d59aba80927ebc52e865bd
zee_zee <zmarie2303@gmail.com>
2021-02-14 20:28:40 -0500

    New portfolio. v02

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-const swup = new Swup()
+$(document).ready(function() {
+    $('#fade').hide();
+    $('#fade2').hide();
+    $('#fade3').hide();
+    $('#fade').slideDown(1500);
+    $('#fade2').slideDown(950);          
+    $('#fade3').slideDown(1500);         
+
+} 
+)    
