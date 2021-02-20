99decbd938631e4fbe4cc88d37a61b9ecf416bbc
zee_zee <zmarie2303@gmail.com>
2021-02-20 16:04:41 -0500

    C

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
     $('#fade').hide();
     $('#fade2').hide();
     $('#fade3').hide();
-    $('#fade').slideDown(1500);
-    $('#fade2').slideDown(950);          
-    $('#fade3').slideDown(1500);         
+    $('#fade').slideDown(3000);
+    $('#fade2').slideDown(2000);          
+    $('#fade3').slideDown(1000);         
 
 } 
 )    
